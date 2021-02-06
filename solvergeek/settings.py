bfaf4d1a1e918b5d34a1b496f7b64fcdc475fd59
Emmanuel-Aggrey <aggrey.en@gmail.com>
2021-02-06 21:13:56 +0000

    .

--- solvergeek/settings.py
+++ solvergeek/settings.py
@@ -287,8 +287,9 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION=config('AWS_LOCATION')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
+
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 STATICFILES_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
