54bf475ff8ecc926513a7309351a30c3fff32b02
Emmanuel-Aggrey <aggrey.en@gmail.com>
2021-02-10 05:54:43 +0000

    swaped to DOS

--- solvergeek/settings.py
+++ solvergeek/settings.py
@@ -218,13 +218,13 @@ CKEDITOR_CONFIGS = {
 }
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
-STATIC_URL = '/static/'
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR,'static')
-]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS =[
+#     os.path.join(BASE_DIR,'static')
+# ]
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -276,13 +276,13 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION=config('AWS_LOCATION')
 
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 MEDIA_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL,'/media/')
 DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
