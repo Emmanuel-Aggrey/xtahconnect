c865dd0bea53e50da0312903d6b964651a8230f9
Emmanuel-Aggrey <aggrey.en@gmail.com>
2021-02-18 22:07:23 +0000

    .

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -9,6 +9,7 @@ from hashids import Hashids
 # Create your models here.
 hashids = Hashids()
 
+
 class Base_Model(models.Model):
     date_add = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now_add=True)
@@ -25,17 +26,23 @@ class Category(Base_Model):
     def get_absolute_url(self):
         return reverse("category_page")
 
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
     def __str__(self):
         return self.name
 
-    class Mata:
-        verbose_name_plural = 'categories'
+    
+
 
 
 class Sub_Category(Base_Model):
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name='sub_categories')
-    name = models.CharField('sub category',max_length=200)
+    name = models.CharField('sub category', max_length=200)
     is_available = models.BooleanField(default=True)
 
     # create a new slug
@@ -58,14 +65,19 @@ class Product(Base_Model):
     name = models.CharField(max_length=100)
     image = models.FileField(upload_to='images/%Y/%m/%d/')
     price = models.DecimalField(decimal_places=2, max_digits=20)
-    description =RichTextField(blank=True, null=True)
+    description = RichTextField(blank=True, null=True)
     is_available = models.BooleanField(default=True)
     slug = models.SlugField(unique=True, editable=False, null=True, blank=True)
-    image1 = models.ImageField(upload_to='images/%Y/%m/%d/',null=True, blank=True)
-    image2 = models.ImageField(upload_to='images/%Y/%m/%d/',null=True, blank=True)
-    image3 = models.ImageField(upload_to='images/%Y/%m/%d/',null=True, blank=True)
-    image4 = models.ImageField(upload_to='images/%Y/%m/%d/',null=True, blank=True)
-    image5 = models.ImageField(upload_to='images/%Y/%m/%d/',null=True, blank=True)
+    image1 = models.ImageField(
+        upload_to='images/%Y/%m/%d/', null=True, blank=True)
+    image2 = models.ImageField(
+        upload_to='images/%Y/%m/%d/', null=True, blank=True)
+    image3 = models.ImageField(
+        upload_to='images/%Y/%m/%d/', null=True, blank=True)
+    image4 = models.ImageField(
+        upload_to='images/%Y/%m/%d/', null=True, blank=True)
+    image5 = models.ImageField(
+        upload_to='images/%Y/%m/%d/', null=True, blank=True)
 
     # create a new slug
 
@@ -80,16 +92,14 @@ class Product(Base_Model):
     def __str__(self):
         return f'{self.name}: {self.category}'
 
-
     def save(self, *args, **kwargs):
         id_ = Product.objects.values_list('pk', flat=True).count()
         id_ = str(id_)
         slug = slugify(self.name)
-        self.slug =  slug+'-'+id_
+        self.slug = slug+'-'+id_
         return super().save(*args, **kwargs)
 
 
-
 # thethella
-# asare_stephen111 
+# asare_stephen111
 # https://theteller.net/documentation
