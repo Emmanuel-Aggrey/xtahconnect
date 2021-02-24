346c5ca8be99261d5222d6f8a2301266200fd5bf
Emmanuel-Aggrey <aggrey.en@gmail.com>
2021-02-24 04:54:21 +0000

    .

--- solvergeek/settings.py
+++ solvergeek/settings.py
@@ -222,8 +222,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 THUMBNAIL_ALIASES = {
@@ -279,8 +279,8 @@ STATICFILES_DIRS = [
 # STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# MEDIA_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL,'/media/')
-# DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL,'/media/')
+DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 
 
 PAYSTACK_SCRET_KEY = config('PAYSTACK_SCRET_KEY')
