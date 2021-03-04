22229dd13a940f964bf08af2f78ba0026bbeffe7
Emmanuel-Aggrey <aggrey.en@gmail.com>
2021-03-04 06:04:28 +0000

    .

--- solvergeek/settings.py
+++ solvergeek/settings.py
@@ -128,13 +128,13 @@ WSGI_APPLICATION = 'solvergeek.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # local posgress db
