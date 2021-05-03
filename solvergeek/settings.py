c396178fc0c7f6b67d8b5245567fc63215cb0dcf
Emmanuel-Aggrey <aggrey.en@gmail.com>
2021-05-03 09:51:33 +0000

    using system storage

--- solvergeek/settings.py
+++ solvergeek/settings.py
@@ -297,13 +297,13 @@ STATICFILES_DIRS = [
 # STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-MEDIA_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL,'/media/')
-DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
+# MEDIA_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL,'/media/')
+# DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 
 
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MAPBOX_KEY =config('MOBOX_API_PRIVATE_TOKEN')  
 
